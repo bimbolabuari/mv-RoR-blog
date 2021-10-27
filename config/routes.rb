b41d3bcb223e1de5a2bac7ea7419fc1fdad04a43
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-10-27 18:24:46 +0100

    chore: Add linter configuration for efficient workflow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "blog_users#user"
+  get "/users/", to: "blog_users#user"
+  get "/users/:id", to: "blog_users#profile"
+  get "/users/:id/posts", to: "blog_posts#posts"
+  get "/users/:id/posts/:id", to: "blog_posts#post"
 end
