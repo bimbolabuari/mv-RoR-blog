8ae14613b4bf87b428f101c564246da1be2c3bb4
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-11-05 11:01:46 +0100

    feat: Add comments and likes form routers

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
+  get "/users/:id/posts/new", to: "posts#new"
+  post "/users/:id/posts", to: "posts#create"
+  post "/users/:id/posts/:id/likes", to: "likes#create"
+  post "/users/:id/posts/:id/comments", to: "comments#create"
+  get "/users/:id/posts/:id/comments/new", to: "comments#new"
 end
