b55dd12f857591daa025196405b0c1b0d341b1ea
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-11-05 21:06:29 +0100

    feat: Add like validation unit test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Abimbola', photo: 'photo',
+                        bio: 'biography of Abimbola',
+                        posts_counter: 6)
+    @post = Post.create(user_id: @user.id, title: 'My post',
+                        text: 'My first post',
+                        comments_counter: 1, likes_counter: 3)
+    @like1 = Like.create(user_id: @user.id, post_id: @post.id)
+    @like2 = Like.create(user_id: @user.id, post_id: @post.id)
+  end
+
+  it 'update_likes_counter method should update the likes counter of a post' do
+    Like.update_like_counter(@post)
+    expect(@post.likes_counter).to be(4)
+  end
 end
