4bc5d77a62a4f536cbd856a1961d116f2b5f1bd0
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-11-05 21:10:07 +0100

    feat: Modify model.rb file to add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :title, length: { maximum: 200 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.update_post_counter(user)
+  def self.update_posts_counter(user)
     count = Post.where("author_id = #{user.id}").count
     user.posts_counter = count
     user.save
