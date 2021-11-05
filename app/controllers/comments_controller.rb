8fe35d57b049f9ed9aa6b1344435bf87ec81aa45
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-11-05 21:10:57 +0100

    feat: Modify post, likes and comment controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,10 @@ class CommentsController < ApplicationController
     @comment.text = params[:comment]['text']
 
     if @comment.save
+      flash[:notice] = 'Comment has been added!'
       redirect_to "/users/#{params[:user_id]}/posts/#{params[:id]}/"
     else
+      flash[:notice] = 'Comment isnt added!'
       redirect_to "/users/#{params[:user_id]}/posts/#{params[:id]}/comments/new"
     end
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,13 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.joins(:author).where(author: { id: @user.id }).order(created_at: :desc)
-    @comments = Comment.order(created_at: :desc)
+    @comments = Comment.includes(:author).order(created_at: :desc)
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @comments = Comment.order(created_at: :desc)
+    @comments = Comment.includes(:author).order(created_at: :desc)
   end
 
   def new
@@ -20,6 +20,13 @@ class PostsController < ApplicationController
   def create
     @post.title = params[:post]['title']
     @post.text = params[:post]['text']
+    @post.author_id = params[:user_id]
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.save
+    Post.update_post_counter(User.find(params[:user_id]))
+    flash[:notice] = 'Post added'
+    redirect_to user_posts_url(@post.author_id)
 
     if @post.save
       redirect_to "/users/#{params[:user_id]}/posts/#{@post.id}"
