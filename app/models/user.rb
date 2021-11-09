06f6cba697fee072e9ed2936862c600cecf7cc5b
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-11-09 19:45:55 +0100

    feat: Uncomment confirmable in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id', class_name: 'Post'
-  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :posts, dependent: :delete_all, foreign_key: 'author_id', class_name: 'Post'
+  has_many :comments, dependent: :delete_all, foreign_key: 'author_id', class_name: 'Comment'
+  has_many :likes, dependent: :delete_all, foreign_key: 'author_id', class_name: 'Like'
 
   def self.recent_posts(user)
     Post.joins(:author).where(author: { id: user.id }).order(created_at: :desc).limit(3)
